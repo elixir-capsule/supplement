de2550b31e779169c1c091ba85b9ce48e22c4b8f
T Floyd Wright <tfwright@gmail.com>
2020-10-06 12:06:27 -0500

    Don't require ex_aws_module to be configured

--- lib/supplement/storages/s3.ex
+++ lib/supplement/storages/s3.ex
@@ -12,7 +12,7 @@ defmodule Capsule.Storages.S3 do
     {:ok, contents} = Upload.contents(upload)
 
     case Client.put_object(config(opts, :bucket), key, contents)
-         |> config(:ex_aws_module).request() do
+         |> ex_aws_module().request() do
       {:ok, _} ->
         encapsulation = %Encapsulation{
           id: key,
@@ -30,7 +30,7 @@ defmodule Capsule.Storages.S3 do
   @impl Storage
   def copy(%Encapsulation{id: id} = encapsulation, path, opts \\ []) do
     case Client.put_object_copy(config(opts, :bucket), path, config(opts, :bucket), id)
-         |> config(:ex_aws_module).request() do
+         |> ex_aws_module().request() do
       {:ok, _} ->
         {:ok, %{encapsulation | id: path}}
 
@@ -42,7 +42,7 @@ defmodule Capsule.Storages.S3 do
   @impl Storage
   def delete(%Encapsulation{id: id}, opts \\ []) do
     case Client.delete_object(config(opts, :bucket), id)
-         |> config(:ex_aws_module).request() do
+         |> ex_aws_module().request() do
       {:ok, _} -> {:ok, nil}
       error -> handle_error(error)
     end
@@ -50,16 +50,21 @@ defmodule Capsule.Storages.S3 do
 
   @impl Storage
   def open(%Encapsulation{id: id}, opts \\ []) do
-    case Client.get_object(config(opts, :bucket), id) |> config(:ex_aws_module).request() do
+    case Client.get_object(config(opts, :bucket), id) |> ex_aws_module().request() do
       {:ok, %{body: contents}} -> {:ok, contents}
       error -> handle_error(error)
     end
   end
 
-  defp config(opts \\ [], key) do
+  defp config(opts, key) do
     Application.fetch_env!(:capsule, __MODULE__)
     |> Keyword.merge(opts)
     |> Keyword.fetch!(key)
+  end
+
+  defp ex_aws_module() do
+    Application.get_env(:capsule, __MODULE__)
+    |> Keyword.get(:ex_aws_module, ExAws)
   end
 
   defp handle_error({:error, error}) do
